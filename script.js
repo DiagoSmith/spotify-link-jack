2b7b1eb44f747873cc27ee8c9861678670c6bcb6
Roberto Floris <rmfloris@gmail.com>
2020-04-19 22:34:26 +0200

    fix: embed link trickery

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ chrome.runtime.onInstalled.addListener(() => console.log("installed"));
 const SPOTIFY_HOSTS = ["https://open.spotify.com", "https://play.spotify.com"];
 
 const domains = {
-  urls: ["*://open.spotify.com/*", "*://play.spotify.com/*"]
+  urls: ["*://open.spotify.com/*", "*://play.spotify.com/*"],
 };
 
 chrome.webRequest.onBeforeRequest.addListener(
-  details => {
+  (details) => {
     const { url } = details;
 
     if (url.includes(SPOTIFY_HOSTS[0]) || url.includes(SPOTIFY_HOSTS[1])) {
--- stripper.js
+++ stripper.js
@@ -1,6 +1,15 @@
 const SPOTIFY_APP_URI = "spotify:";
 
-const convertUrl = url => {
+// const MEDIA_TYPES = ["track", "album", "playlist"];
+
+// const urlIsHot = (url) => {
+//   return url.includes("");
+// };
+
+const isUrlEmbedded = (url) => url.includes("embed");
+
+const convertUrl = (url) => {
+  if (isUrlEmbedded(url)) return;
   const trimmedUrl = url.slice(url.indexOf("/", 8) + 1, url.indexOf("?"));
   const splitURL = trimmedUrl.split("/");
   return `${SPOTIFY_APP_URI}${splitURL[0]}:${splitURL[1]}`;
